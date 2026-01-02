6bc7722a5f8653f9956b6c0c1de675d4b7b816a8
Herman Haidin <haidinherman@gmail.com>
2026-01-02 22:48:29 +0200

    add initial xcode project

---
+++ lowercase/lowercase/lowercaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  lowercaseApp.swift
+//  lowercase
+//
+//  Created by Herman Haidin on 02.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct lowercaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
