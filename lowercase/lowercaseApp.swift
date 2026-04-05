2ed645ffedc56601eaaa0ca391a32fa86b3ac850
Herman <haidinherman@gmail.com>
2026-04-05 15:06:04 +0300

    Initial Commit

---
+++ lowercase/lowercaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  lowercaseApp.swift
+//  lowercase
+//
+//  Created by Herman Haidin on 05.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct lowercaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
